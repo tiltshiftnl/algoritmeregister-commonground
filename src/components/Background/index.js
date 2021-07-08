6504443ac4b37a1a43563802517b0f89a170d4fd
Johan Groenen <johan.groenen+github@gmail.com>
2021-07-08 11:59:56 +0200

    Initial commit

---
+++ src/components/Background/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/macro'
+
+const Background = styled.div`
+  background-color: ${(p) => p.backgroundColor || "#ffffff"};
+   color: ${(p) => p.color || "#000000"};
+  color: black;
+
+  a {
+    color: ${(p) => p.color || "#000000"};
+  }
+`
+
+export default Background
---
+++ src/components/DemodamLogo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import conduction from '../Logos/images/conduction.png'
+
+const DemodamLogo = (props) => (
+
+
+  // <img style={{background: 'white'}} src="https://www.utrecht.nl/typo3conf/ext/alternet_sitepackage/Resources/Public/Images/svg/wapen-utrecht-rood.svg" alt="Wapen van de Gemeente Utrecht"/>
+  <img src={conduction} height="60" alt="Logo Gemeente Utrecht" />
+
+)
+
+export default DemodamLogo
